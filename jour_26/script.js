3e9ed374168a54e9547ac371cf4a36dba7dcd8ee
rodrigue740 <tchindarodrigue740@gmail.com>
2024-08-17 10:33:19 +0100

    jou26 correction des erreur tu projet tutorial

--- jour_26/script.js
+++ jour_26/script.js
@@ -3,7 +3,7 @@ const slideRight = document.querySelector('.right-slide')
 const slideLeft = document.querySelector('.left-slide')
 const upButton = document.querySelector('.up-button')
 const downButton = document.querySelector('.down-button')
-const slidesLength = slideRight.querySelector("div").length
+const slidesLength = slideRight.querySelectorAll("div").length
 
 let activeSlideIndex = 0
 
